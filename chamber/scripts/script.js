b1d9fca9adfb764319ad74fd569368ca75bc908b
Ann2310-c <amosqueramonje@byupathway.edu>
2026-03-21 18:54:59 -0500

    w02 completed

--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -1,50 +1,44 @@
-const container = document.querySelector("#members");
+const membersContainer = document.querySelector("#members");
 const menuBtn = document.querySelector("#menuBtn");
 const navMenu = document.querySelector("#navMenu");
 
 menuBtn.addEventListener("click", () => {
-  navMenu.classList.toggle("open");
+    navMenu.classList.toggle("open");
 });
 
-// FETCH JSON
 async function getMembers() {
-  const response = await fetch("data/members.json");
-  const data = await response.json();
-  displayMembers(data);
+    const response = await fetch("data/members.json");
+    const data = await response.json();
+    displayMembers(data);
 }
 
-// DISPLAY
 function displayMembers(members) {
-  container.innerHTML = "";
-
-  members.forEach(member => {
-    const card = document.createElement("section");
-
-    card.innerHTML = `
-    <img src="images/${member.image}" alt="${member.name}">
-    <h2>${member.name}</h2>
-    <p>${member.address}</p>
-    <p>${member.phone}</p>
-    <a href="${member.website}" target="_blank">Website</a>`;
-
-    container.appendChild(card);
+    membersContainer.innerHTML = "";
+    members.forEach(member => {
+        const card = document.createElement("section");
+        card.innerHTML = `
+        <img src="images/${member.image}" alt="Image of ${member.name}">
+        <h2>${member.name}</h2>
+        <p>${member.address}</p>
+        <p>${member.phone}</p>
+        <a href="${member.website}" target="_blank">Visit Website</a>`;
+        membersContainer.appendChild(card);
   });
 }
 
-// TOGGLE
 document.getElementById("grid").addEventListener("click", () => {
-  container.classList.add("grid");
-  container.classList.remove("list");
+    membersContainer.classList.add("grid");
+    membersContainer.classList.remove("list");
 });
 
 document.getElementById("list").addEventListener("click", () => {
-  container.classList.add("list");
-  container.classList.remove("grid");
+    membersContainer.classList.add("list");
+    membersContainer.classList.remove("grid");
 });
 
-// FOOTER
+
 document.getElementById("year").textContent = new Date().getFullYear();
 document.getElementById("lastModified").textContent = document.lastModified;
 
-// RUN
+
 getMembers();

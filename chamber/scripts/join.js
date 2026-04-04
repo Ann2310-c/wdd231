7ec85cd4679e0cfaeff74fc2b80af9a45b26a0b4
Ann2310-c <amosqueramonje@byupathway.edu>
2026-04-04 12:07:41 -0500

    w04

---
+++ chamber/scripts/join.js
@@ -0,0 +1,20 @@
+// TIMESTAMP
+document.getElementById("timestamp").value = new Date().toISOString();
+
+// MODALS
+const links = document.querySelectorAll("[data-modal]");
+const dialogs = document.querySelectorAll("dialog");
+
+links.forEach(link => {
+  link.addEventListener("click", (e) => {
+    e.preventDefault();
+    const modal = document.getElementById(link.dataset.modal);
+    modal.showModal();
+  });
+});
+
+dialogs.forEach(dialog => {
+  dialog.querySelector(".close").addEventListener("click", () => {
+    dialog.close();
+  });
+});
